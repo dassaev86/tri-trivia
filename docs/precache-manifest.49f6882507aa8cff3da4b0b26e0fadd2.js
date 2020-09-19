40236d7ca45dfad991daf4710085a253d6e9edb6
Dassaev Novela <dassaev86@gmail.com>
2020-09-19 13:30:42 -0500

    Docs agregado

---
+++ docs/precache-manifest.49f6882507aa8cff3da4b0b26e0fadd2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5157e8e82bde6707754adac2bd422d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f7851e96ec7289323d",
+    "url": "/static/css/2.e603e8eb.chunk.css"
+  },
+  {
+    "revision": "2fda6956d6e1236cac70",
+    "url": "/static/css/main.ab867967.chunk.css"
+  },
+  {
+    "revision": "d3f7851e96ec7289323d",
+    "url": "/static/js/2.2b8c5218.chunk.js"
+  },
+  {
+    "revision": "0fd07708345db41e7462430257b87932",
+    "url": "/static/js/2.2b8c5218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fda6956d6e1236cac70",
+    "url": "/static/js/main.0a677280.chunk.js"
+  },
+  {
+    "revision": "4e7be396c02193fbe90a",
+    "url": "/static/js/runtime-main.f2e9d414.js"
+  },
+  {
+    "revision": "d468138d8a56eb0199b1b9075bc72fb2",
+    "url": "/static/media/jeopardy.d468138d.jpg"
+  },
+  {
+    "revision": "7fad7bcf74b5612a39aea5b09d91e15e",
+    "url": "/static/media/strikeout.7fad7bcf.jpg"
+  },
+  {
+    "revision": "503b76bd991c7d4ec889c9a86893042f",
+    "url": "/static/media/traditional_trivia.503b76bd.jpg"
+  }
+]);
